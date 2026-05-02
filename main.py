898d7acdb5de5dd1396de34cd991356d86cea54c
ABC-JR <ertensabakioo@gmail.com>
2026-05-02 16:46:13 +0500

    Fix: remove circular import from core/model.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ try:
     from models.user import Base, User
     from models.favorite import Favorite
     from models.song import Song
-    from routes import auth, cheker
+    from routes import auth, cheker , retrain
     from database.database import engine
 except Exception as e:
     traceback.print_exc()
@@ -21,7 +21,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(auth.router, prefix="/auth")
 app.include_router(cheker.router, prefix="/check")
-# app.include_router(retrain.router, prefix="/retrain")
+app.include_router(retrain.router, prefix="/retrain")
 # app.include_router(file.router, prefix="/file")
 
 app.add_middleware(
