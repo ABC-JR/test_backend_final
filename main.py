6b0dda2d02552e56e7e679a561d5fc6befd9b9f6
ABC-JR <ertensabakioo@gmail.com>
2026-05-02 15:09:47 +0500

    checking

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 )
 
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
