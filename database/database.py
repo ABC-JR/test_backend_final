80e8c21a82e1f8072dc08fece47445a289d0ee2a
ABC-JR <ertensabakioo@gmail.com>
2026-05-02 15:21:10 +0500

    checking

--- database/database.py
+++ database/database.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-# load_dotenv()
-local_url =  "postgresql://pmdb_8ikr_user:eeFMRQrYTWzfUhJctZRW5CbfbJz1vVit@dpg-d7o30hpf9bms738rsgn0-a.oregon-postgres.render.com/pmdb_8ikr"
+load_dotenv()
+database_url = os.getenv("DATABASE_URL")
 
+if not database_url:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
-
-
-engine = create_engine(local_url)
+engine = create_engine(database_url)
 
 sessionlocal = sessionmaker(bind=engine , autocommit=False , autoflush=False)
 
